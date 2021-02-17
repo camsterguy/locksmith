c35d30cdb2f359f74a97a138388fe0a5d8afaa11
Cam Vahe <cvahe@Cams-MacBook-Pro.local>
2021-02-17 10:17:11 -0500

    selenium

--- criticalFunctions.py
+++ criticalFunctions.py
@@ -7,6 +7,7 @@ from datetime import date
 from urllib.request import Request, urlopen
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import requests,os
 
 today = date.today()
 thismonth = today.strftime("%b")
@@ -19,6 +20,21 @@ spreads = []
 awayteams = []
 hometeams = []
 
+def setupSelenium(url):
+    global soup
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.headless = True
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36")
+    options.add_argument("--window-size=1920,1200")
+    options.add_experimental_option('useAutomationExtension', False)
+    options.add_argument("--disable-blink-features=AutomationControlled")
+    driver = webdriver.Chrome(options=options, executable_path='./drivers/chromedriver')
+    driver.get(url)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.quit()
+
 def setupHTTP(initURL):
     global soup
     ctx = ssl.create_default_context()
@@ -28,13 +44,14 @@ def setupHTTP(initURL):
     soup = BeautifulSoup(html, 'html.parser')
 
 def setup403(initURL):
+    global soup
     hdr = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; Win64; x64)' }
     ctx = ssl.create_default_context()
     ctx.check_hostname = False
     ctx.verify_mode = ssl.CERT_NONE
     req = urllib.request.Request(initURL, headers=hdr)
     response = urllib.request.urlopen(req, context=ctx).read()
-    soup = BeautifulSoup(response, 'lxml')
+    soup = BeautifulSoup(response, 'html.parser')
 
 
 #Usage: get_Winners("https://www.basketball-reference.com/boxscores/index.fcgi?month="+month+"&day="+day+"&year="+year+"")
@@ -65,12 +82,17 @@ def get_Games(url):
             awayteams.append(awayabrev)
             hometeams.append(homeabrev)
                                             
-def get_Spreads(url):
-    setupHTTP(url)
-    table_rows = soup.find_all('div', class_='game-line__home-line d-flex justify-content-around')
-    for game in table_rows:
-        odds = game.find('button')
-        print(odds.get_text()[:4])
+def get_Spreads(year, month, day):
+    dayurl=("https://www.sportsbookreview.com/betting-odds/nba-basketball/?date="+year+month+day)
+    print(dayurl)
+    setupSelenium(dayurl)
+    games = soup.find_all('div', class_='columnsContainer-3tVf9')
+    for game in games:
+        odds = game.find('span', class_='adjust-1uDgI')
+        try:
+            spreads.append(odds.text)
+        except:
+            continue
 
     
 
--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from datetime import date
 today = date.today()
 month = str((today.strftime("%B"))).lower()
 year = (today.strftime("%Y"))
-scheduleURL = "https://www.basketball-reference.com/leagues/NBA_"+year+"_games-"+month+".html"
-spreadsURL = "https://mybookie.ag/sportsbook/nba/"
 
-print("Today's Games:",today)
+
+scheduleURL = "https://www.basketball-reference.com/leagues/NBA_"+year+"_games-"+month+".html"
+
+
+crit.get_Spreads("2021", "02", "16")
+from criticalFunctions import spreads
+
+print(spreads)
 
 crit.get_Games(scheduleURL)
-crit.get_Spreads(spreadsURL)
-
-from criticalFunctions import hometeams, spreads, awayteams
-#print(hometeams, spreads, awayteams)
+from criticalFunctions import hometeams, awayteams
+print(hometeams, awayteams)
