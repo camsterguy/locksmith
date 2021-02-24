00fe6b800610019ca3b511c371b993f727f86cc2
Cam Vahe <cvahe@Cams-MacBook-Pro.local>
2021-02-24 09:43:45 -0500

    fixed SAS

--- criticalFunctions.py
+++ criticalFunctions.py
@@ -20,7 +20,7 @@ gamedate = thismonth+" "+thisday+", "+thisyear
 spreads = []
 
 def setupSelenium(initurl):
-    url = initurl.replace('NYK','NY').replace('NOP','NO').replace('GSW','GS').replace('PHX','PHO')
+    url = initurl.replace('NYK','NY').replace('NOP','NO').replace('GSW','GS').replace('PHX','PHO').replace('SAS','SA')
     global soup
     global driver
     options = webdriver.ChromeOptions()
